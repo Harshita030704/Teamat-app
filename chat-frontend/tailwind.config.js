13414bc549f411924400c0827f13d9aa99967dbe
Harshita Chaurasia <143120932+Harshita030704@users.noreply.github.com>
2026-08-16 13:49:13 +0530

    Add files via upload

---
+++ chat-frontend/tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  darkMode: "class",
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        space: ["Space Grotesk", "sans-serif"],  // ⭐ Add custom font here
+      },
+    },
+  },
+  plugins: [],
+};
